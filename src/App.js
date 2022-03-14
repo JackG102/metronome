0b2806fc242cfcd27cb93f6ddb4cbf65f327f8e5
Jack Garratt <jack.garratt@icfnext.com>
2022-03-14 13:21:53 -0400

    Added skeleton metronome component to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Metronome from './components/Metronome/Metronome';
 
 const App = () => {
   return (
     <div className="ui raised very padded text container segment">
       <h1>Metronome App</h1>
+      <Metronome />
     </div>
   );
 }
---
+++ src/components/Metronome/Metronome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Metronome = () => {
+  return (
+    <div>
+      <h2>Metronome Component</h2>
+    </div>
+  );
+}
+
+export default Metronome;
