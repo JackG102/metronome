21e870884f6115aead99aff5894ef1b1273c2e9e
Jack Garratt <jack.garratt@icfnext.com>
2022-03-14 13:44:58 -0400

    Added slider and controlled it

--- src/components/Metronome/Metronome.js
+++ src/components/Metronome/Metronome.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const Metronome = () => {
+  const [bpm, setBPM] = useState('60');
+
   return (
     <div>
       <h2>Metronome Component</h2>
+      <input 
+        className="slider" 
+        id="myRange"
+        type="range" 
+        min="1" 
+        max="240"
+        onChange={e => setBPM(e.target.value)}
+        >
+      </input>
+    <h3>Beats per Minute: {bpm}</h3>
     </div>
   );
 }
