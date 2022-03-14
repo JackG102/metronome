9ee435f526837bc3133c7d15dc1516757dd81ec3
Jack Garratt <jack.garratt@icfnext.com>
2022-03-14 14:12:38 -0400

    Set up pieces of state for toggling functionality and bpm

--- src/components/Metronome/Metronome.js
+++ src/components/Metronome/Metronome.js
@@ -3,20 +3,37 @@ import ReactDOM from 'react-dom';
 
 const Metronome = () => {
   const [bpm, setBPM] = useState('60');
+  const [running, setRunning] = useState(false);
+
+  const toggleRunning = (event) => {
+    event.preventDefault();
+    running ? setRunning(false) : setRunning(true);
+  };
 
   return (
     <div>
       <h2>Metronome Component</h2>
-      <input 
-        className="slider" 
-        id="myRange"
-        type="range" 
-        min="1" 
-        max="240"
-        onChange={e => setBPM(e.target.value)}
+      <form>
+        <input 
+          className="slider" 
+          id="myRange"
+          type="range" 
+          min="1" 
+          max="240"
+          onChange={e => setBPM(e.target.value)}
+          >
+        </input>
+      <div>
+        <button 
+          className="ui button green" 
+          type="submit"
+          onClick={toggleRunning}
         >
-      </input>
-    <h3>Beats per Minute: {bpm}</h3>
+          {running ? 'Stop' : 'Start'}
+        </button>
+      </div>
+      </form>
+      <h3>Beats per Minute: {bpm}</h3>
     </div>
   );
 }
